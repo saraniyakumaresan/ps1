1d2c924ac43a17af62524a4515f1d306a5fd44c6
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-06-22 20:09:10 +0530

    Add files via upload

---
+++ powershell220.ps1
@@ -0,0 +1,7 @@
+param (
+    $param3,
+    $param4
+)
+Write-Host "Parameter 1: $param3"
+Write-Host "Parameter 2: $param4"
+Write-Host "##gbStart##Appstore##splitKeyValue##TestingAppstore##gbEnd##"
